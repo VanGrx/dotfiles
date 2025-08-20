f717349b2ed6f00dc6e3ccb3a7188175cfaad74e
Igor Grkavac <grkavac.igor@gmail.com>
2025-08-20 22:59:16 +0100

    Add inline git blame

--- init.lua
+++ init.lua
@@ -303,6 +303,29 @@ require("lazy").setup({
 		},
 		version = "^1.0.0", -- optional: only update when a new 1.x version is released
 	},
+
+	{ -- Adds git releated signs to the gutter, as well as utilities for managing changes
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+				delay = 10,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+		},
+	},
 	-- NOTE: Plugins can also be configured to run Lua code when they are loaded.
 	--
 	-- This is often very useful to both group configuration, as well as handle
